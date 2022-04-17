ea12770618128e0a2370bf966e6511c38627fcca
RayruiHu <789.plm@163.com>
2022-04-17 18:44:03 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template(template_name_or_list='index.html', name=name, movies=movies)
+
+@app.route('/home')
+def hello():
+    return 'Welcom to My Watchlist!'
+
+# url里支持定义变量
+@app.route('/home/<name>')
+def user_page(name):
+    return 'User: %s'%name
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='li'))
+    print(url_for('test_url_for'))
+    # 若调用多余关键字参数，会被作为查询字符串附加到URL后
+    print(url_for('test_url_for', num=2))
+    return 'Test Page'
+
+name = '小糊'
+movies = [{'title': 'My Neighbor Totoro', 'year': '1988'},
+          {'title': 'Dead Poets Society', 'year': '1989'},
+          {'title': 'A Perfect World', 'year': '1993'},
+          {'title': 'Leon', 'year': '1994'},
+          {'title': 'Mahjong', 'year': '1996'},
+          {'title': 'Swallowtail Butterfly', 'year': '1996'},
+          {'title': 'King of Comedy', 'year': '1999'},
+          {'title': 'Devils on the Doorstep', 'year': '1999'},
+          {'title': 'WALL-E', 'year': '2008'},
+          {'title': 'The Pork of Music', 'year': '2012'}]
